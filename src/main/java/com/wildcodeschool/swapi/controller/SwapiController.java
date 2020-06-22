93973a14990ad678049bce3861b9d70444b06d34
swann <chevreauswann@outlook.fr>
2020-06-22 12:35:27 +0200

    quête api webClient

--- src/main/java/com/wildcodeschool/swapi/controller/SwapiController.java
+++ src/main/java/com/wildcodeschool/swapi/controller/SwapiController.java
@@ -24,9 +24,25 @@ public class SwapiController {
     @GetMapping("/planet")
     public String planet(Model model, @RequestParam Long id) {
 
-        Planet planetObject = null;
-        // TODO : call the API and retrieve the planet
 
+        // TODO : call the API and retrieve the planet
+        WebClient webClient = WebClient.create(SWAPI_URL);
+        Mono<String> call = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/planets/{id}/")
+                        .build(id))
+                .retrieve()
+                .bodyToMono(String.class);
+
+        String response = call.block();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Planet planetObject = null;
+        try {
+            planetObject = objectMapper.readValue(response, Planet.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
         model.addAttribute("planetInfos", planetObject);
 
         return "planet";
--- src/main/java/com/wildcodeschool/swapi/model/Planet.java
+++ src/main/java/com/wildcodeschool/swapi/model/Planet.java
@@ -5,9 +5,138 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Planet {
 
     // TODO : add attributes
+    private String name;
+    @JsonProperty("rotation_period")
+    private Integer rotationPeriod;
+    @JsonProperty("orbital_period")
+    private Integer orbitalPeriod;
+    private Integer diameter;
+    private String climate;
+    private String gravity;
+    private String terrain;
+    @JsonProperty("surface_water")
+    private Integer surfaceWater;
+    private Integer population;
 
+    public Integer getRotationPeriod() {
+        return rotationPeriod;
+    }
+
+    public void setRotationPeriod(Integer rotationPeriod) {
+        this.rotationPeriod = rotationPeriod;
+    }
+
+    public Integer getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public void setOrbitalPeriod(Integer orbitalPeriod) {
+        this.orbitalPeriod = orbitalPeriod;
+    }
+
+    public Integer getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(Integer diameter) {
+        this.diameter = diameter;
+    }
+
+    public String getClimate() {
+        return climate;
+    }
+
+    public void setClimate(String climate) {
+        this.climate = climate;
+    }
+
+    public String getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(String gravity) {
+        this.gravity = gravity;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(String terrain) {
+        this.terrain = terrain;
+    }
+
+    public Integer getSurfaceWater() {
+        return surfaceWater;
+    }
+
+    public void setSurfaceWater(Integer surfaceWater) {
+        this.surfaceWater = surfaceWater;
+    }
+
+    public String[] getResidents() {
+        return residents;
+    }
+
+    public void setResidents(String[] residents) {
+        this.residents = residents;
+    }
+
+    public String[] getFilms() {
+        return films;
+    }
+
+    public void setFilms(String[] films) {
+        this.films = films;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getEdited() {
+        return edited;
+    }
+
+    public void setEdited(String edited) {
+        this.edited = edited;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private String[] residents;
+    private String[] films;
+    private String created;
+    private String edited;
+    private String url;
     // TODO : add an empty constructor
+    public Planet() {
 
+    }
     // TODO : add getters and setters
+    public String getName() {
+        return this.name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPopulation() {
+        return this.population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
 }
